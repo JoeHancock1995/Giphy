b6799f0be22fa2168e8cc643a41281124e9aa990
Joe Hancock <joe.hancock95@gmail.com>
2019-07-01 20:36:37 -0500

    more js work, loop made

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 
 
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZJqlCvnXXccVUVpatKWlVNZ959QmuuHE&limit=10");
-xhr.done(function(data) { console.log("success got data", data); });
+var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=cats&api_key=ZJqlCvnXXccVUVpatKWlVNZ959QmuuHE&limit=10");
+
+xhr.done(function(response) { 
+    console.log("success got data", response); 
+
+var jiffs = response.data
+
+for (i in jiffs)
+{
+    $('.inner').append("<img src='"+jiffs[i].images.original.url+"';/>")
+}
+    });
