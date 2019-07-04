af5464a25b1ff8588c99dd47cb0b50ff8302a684
Joe Hancock <joe.hancock95@gmail.com>
2019-07-04 11:06:45 -0500

    code change for improved layout, simplicity with better understanding, making buttons js generated

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
-function getData(){
-  var input = $("#searchtext").val()
-  var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZJqlCvnXXccVUVpatKWlVNZ959QmuuHE&limit=10");
-  xhr.done(function(response) { 
-      console.log("success got data", response); 
-      var jiffs = response.data
-      for (i in jiffs)
-      {
-          $('.inner').append("<img src='"+jiffs[i].images.original.url+"' style='height:350px; width:350px;'/>")
-      }
-          });
+var topics = ["glitchart" , "static" , "synthesis" , "pixelsorting" , "brutalism"
+ , "pixelmosh" , "databend" , "webpunk" , "analogglitch" , "videosynth" , "nihilminus"];
+
+function addSearchBtns() {
+  $("#buttons").html("");
+  for (i=0; i < topics.length; i++) {
+    var $button = $("<input type='button' class= 'btn btn-sm search-btn' />");
+    $button.val(topics[i]);
+    $("#buttons").append($button);
   }
+}
 
-//////// buttons /////////////
 
-$("button").on("click", function() {
-  var person = $(this).attr("data-person");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=ZJqlCvnXXccVUVpatKWlVNZ959QmuuHE";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
- 
-    .then(function(response) {
-      var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-        if (results[i].rating !== "" ) {
-        
-      var gifDiv = $("<div>");
-      var rating = results[i].rating;
-      var p = $("<p>").text("Rating: " + rating);
-      var personImage = $("<img>");
+  
+$("button").on("click", function(){
+ var digital = $(this).attr("data-digital");
+ var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + 
+ digital + "&api_key=ZJqlCvnXXccVUVpatKWlVNZ959QmuuHE&limit=10"; 
 
-          personImage.attr("src", results[i].images.original.url);
-          gifDiv.append(p);
-          gifDiv.append(personImage);
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      }
-    });
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ })
+
+ .then(function(response){
+   console.log(queryURL);
+   console.log(response);
+
+   var results = response.data;
+   for (var i = 0; i < results.length; i++) {
+     var digitalDiv = $("<div>");
+
+     var digitalImage = $("<img>");
+     digitalImage.attr("src", results[i].images.fixed_height.url);
+     digitalDiv.append(digitalImage);
+
+     $("#gifs-here").prepend(digitalDiv);
+   }
+ });
 });
+
